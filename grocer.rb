99d8d814ebe001ecf39bf4df5ee3a67be8dbbbb2
Jared Harbison <jared.harbison@gmail.com>
2019-01-13 02:01:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,4 +42,12 @@ def apply_coupons(cart, coupons)
 end	
 
 def checkout(cart, coupons)
+cart = consolidate_cart(cart: cart)
+cart = apply_coupons(cart: cart, coupons: coupons)
+  cart = apply_clearance(cart: cart)
+  result = 0
+  cart.each do |food, info|
+    result += (info[:price] * info[:count]).to_f
+  end
+  result > 100 ? result * 0.9 : result
 end
