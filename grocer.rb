8b2cad2e994b0c97365f083a19869b8c39565f4d
Jared Harbison <jared.harbison@gmail.com>
2019-01-13 02:04:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,5 +41,17 @@ def apply_coupons(cart, coupons)
   result
 end	
 
-def checkout(cart, coupons)
+def checkout(items, coupons)
+  cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
+  
+  total = 0
+  
+  cart2.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
   end
+  
+  total > 100 ? total * 0.9 : total
+  
+end
